6657caaf68680cca2b5a8710cea684b5cb698aaa
Hari Prasad Singh <shariprasad983@gmail.com>
2024-02-03 15:42:52 +0530

    add console log for image url

--- src/api/openAI.js
+++ src/api/openAI.js
@@ -77,7 +77,7 @@ const dalleApiCall = async (prompt, messages)=>{
         })
 
         let url = res?.data?.data[0]?.url;
-        // console.log('got image url: ',url);
+        console.log('got image url: ',url);
         messages.push({role: 'assistant', content: url});
         return Promise.resolve({success: true, data: messages});
 
